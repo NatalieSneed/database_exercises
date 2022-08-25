a2658e0a116e071b8aa79fa6fd653555a6a23bce
NatalieSneed <106278703+NatalieSneed@users.noreply.github.com>
2022-08-25 16:00:08 -0500

    query updates made

---
+++ albums_seeder.sql
@@ -0,0 +1,34 @@
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Led Zeppelin', 'Led Zeppelin IV', '1971', '30.4', 'Hard rock'),
+       ('Michael Jackson', 'Bad', '1987', '21.2', 'Pop'),
+       ('Meat Loaf', 'Bat Out of Hell', '1987', '22', 'Hard rock'),
+       ('Alanis Morissette', 'Jagged Little Pill', '1995', '25.4', 'Alternative rock'),
+       ('Dirty Dancing', 'Dirty Dancing', '1987', '24.1', 'Pop'),
+       ('Celine Dion', 'Falling into You', '1996', '21.1', 'Pop'),
+       ('Adele', '21', '2011', '27.1', 'Pop'),
+       ('The Beatles', '1', '2000', '23.2', 'Rock'),
+       ('Metallica', 'Metallica', 	'1991', '22.7', 'Heavy metal'),
+('Celine Dion', 'Let''s Talk About Love', '1997', '19.5', 'Soft rock'),
+('Guns N'' Roses', 	'Appetite for Destruction', '1987', '22.8', 'Hard rock'),
+('Bob Marley & The Wailers', 'Legend: The Best of Bob Marley & The Wailers', '1984', '22.9', 'Reggae'),
+('Bruce Springsteen', 'Born in the U.S.A.', '1984', '22', 'Heartland rock'),
+('ABBA', 'Gold: Greatest Hits', '1992', '21.6', 'Pop, disco'),
+('Dire Straits', 'Brothers in Arms', '1985', '21.1', 'blues rock'),
+('Santana', 'Supernatural', '1999', '20.8',	'Latin rock'),
+       ('Michael Jackson', 'Dangerous', '1991', '17.0', 'pop'),
+        ('Madonna',	'The Immaculate Collection', '1990', '19.8', 'Pop'),
+       ('The Beatles', 'Abbey Road', '1969', '16.8','Rock'),
+       ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', '1967', '18.3', 'Rock'),
+        ('Pink Floyd', 'The Wall', '1979', '18.9',	'Progressive rock');
+
+
+
+
+
+
+
+
+
+
+
+
